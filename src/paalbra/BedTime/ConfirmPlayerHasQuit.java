1a95f05607dc67fec3114942ac6899cf6030d3ca
Paal Braathen <paalbra@gmail.com>
2014-02-20 14:59:38 +0100

    Code cleanup

--- src/paalbra/BedTime/ConfirmPlayerHasQuit.java
+++ src/paalbra/BedTime/ConfirmPlayerHasQuit.java
@@ -9,9 +9,7 @@ public class ConfirmPlayerHasQuit implements Runnable {
 	int numRepeat;
 
 	public ConfirmPlayerHasQuit(BedTime plugin, Player player) {
-		this.plugin = plugin;
-		this.player = player;
-		this.numRepeat = 10;
+		this(plugin, player, 10);
 	}
 
 	public ConfirmPlayerHasQuit(BedTime plugin, Player player, int numRepeat) {
@@ -25,7 +23,6 @@ public class ConfirmPlayerHasQuit implements Runnable {
 		if (!player.isOnline() && !plugin.getServer().getWorld(player.getWorld().getUID()).getPlayers().contains(player)) {
 			plugin.getServer().getPluginManager().callEvent(new PlayerHasQuitEvent(player));
 		} else if (numRepeat > 0) {
-			//plugin.getServer().getScheduler().scheduleAsyncDelayedTask(plugin, new ConfirmPlayerHasQuit(plugin, player, numRepeat - 1), 5L);
 			plugin.getServer().getScheduler().runTaskLaterAsynchronously(plugin, new ConfirmPlayerHasQuit(plugin, player, numRepeat - 1), 5L);
 		} else {
 			plugin.log.warning(plugin.prefix + "Player(" + player.getName() + ") has not become offline even though he should have");
