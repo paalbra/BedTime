487c2cb497b4e975fd9591af2d854c4c8ab77c81
Paal Braathen <paalbra@gmail.com>
2020-12-20 13:25:47 +0100

    Add version and commit hash to filename

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -9,15 +9,18 @@ REPO_URI="$1"
 
 git clone "$REPO_URI" repo && \
 cd repo && \
+GIT_SHORTCOMMIT=$(cat .git/refs/heads/master | cut -c 1-7) && \
 mkdir build && \
 javac -d build -classpath /buildtools/spigot.jar src/paalbra/BedTime/*.java && \
 cp *.yml build && \
 jar -cvf build/BedTime.jar -C build . && \
-cp build/BedTime.jar /output  && \
-cat plugin.yml | grep -oP '(?<=^version: ).*' > /output/bedtime_version && \
+SPIGOT_VERSION=$(cat /output/spigot_version) && \
+BEDTIME_VERSION=$(cat plugin.yml | grep -oP '(?<=^version: ).*' | tee /output/bedtime_version) && \
+BEDTIME_PATH="/output/BedTime-$BEDTIME_VERSION-$GIT_SHORTCOMMIT.jar"
+cp build/BedTime.jar "$BEDTIME_PATH" && \
 cd .. && \
-rm -rf repo
+rm -rf repo && \
 
-echo "Spigot version: $(cat /output/spigot_version)"
-echo "BedTime version: $(cat /output/bedtime_version)"
-echo "BedTime: /output/BedTime.jar"
+echo "Spigot version: $SPIGOT_VERSION"
+echo "BedTime version: $BEDTIME_VERSION"
+echo "BedTime: $BEDTIME_PATH"
